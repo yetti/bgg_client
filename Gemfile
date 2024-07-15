0dc7b9e2cf585499b43817de9721b91c1e727f65
Yetrina Battad <hello@yetti.io>
2024-07-15 23:51:41 +1000

    ci: configure coverage reporting

--- Gemfile
+++ Gemfile
@@ -16,3 +16,6 @@ gem "rubocop-rspec", "~> 3.0", require: false
 gem "rubocop-performance", "~> 1.21", require: false
 
 gem "standard", ">= 1.35.1", require: false
+
+gem "simplecov", "~> 0.22.0"
+gem "simplecov-cobertura", "~> 2.1"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,28 @@
 
 require "bgg_client"
 
+require "simplecov"
+require "simplecov-cobertura"
+
+SimpleCov.configure do
+  formatter SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::CoberturaFormatter
+  ])
+  add_filter do |source_file|
+    source_file.lines.count < 10
+  end
+  add_filter "spec"
+  add_filter "bin"
+
+  add_group "Libraries", "lib"
+
+  minimum_coverage line: 80
+  maximum_coverage_drop 5
+end
+
+SimpleCov.start if ENV.fetch("CI", false) == "true"
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
